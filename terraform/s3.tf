a35e5aa7ea2b1e7765c0e10ffd038deb26b46198
Samuel Adetunji <guysnacho2@gmail.com>
2024-07-17 02:05:23 -0500

    Inject bucket name from variable, add policy update perms

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,10 +1,26 @@
 module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "4.1.2"
-  bucket  = "black-belt-init-ssg-s3"
+  bucket  = var.bucket-name
 
-
-  block_public_acls       = true
-  restrict_public_buckets = false
-  force_destroy           = true
+  policy = jsonencode({
+    "Version" : "2024-07-17",
+    "Statement" : [
+      {
+        "Sid" : "PublicReadGetObject",
+        "Effect" : "Allow",
+        "Principal" : "*",
+        "Action" : [
+          "s3:GetObject"
+        ],
+        "Resource" : [
+          "arn:aws:s3:::${var.bucket-name}/*"
+        ]
+      }
+    ]
+  })
+  block_public_acls        = true
+  restrict_public_buckets  = false
+  control_object_ownership = false
+  force_destroy            = true
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "bucket-name" {
+  description = "The name of our bucket"
+  type        = string
+  default     = "bucketName"
+  sensitive   = true
+}
