5a9547b84263aca999baaeea4a214a183ce7077f
Samuel Adetunji <guysnacho2@gmail.com>
2024-07-17 05:42:55 -0500

    Fix policy and bucket config

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -7,23 +7,20 @@ module "s3-bucket" {
     "Version" : "2012-10-17",
     "Statement" : [
       {
-        "Version" : "2012-10-17",
-        "Statement" : [
-          {
-            "Sid" : "PublicReadGetObject",
-            "Effect" : "Allow",
-            "Principal" : "*",
-            "Action" : "s3:GetObject",
-            "Resource" : "arn:aws:s3:::${var.bucket-name}/*"
-          }
-        ]
+        "Sid" : "PublicReadGetObject",
+        "Effect" : "Allow",
+        "Principal" : "*",
+        "Action" : "s3:GetObject",
+        "Resource" : "arn:aws:s3:::${var.bucket-name}/*"
       }
     ]
   })
-  block_public_acls       = false
-  attach_public_policy    = true
-  restrict_public_buckets = false
-  object_ownership        = "BucketOwnerPreferred"
 
-  force_destroy = true
+  acl                      = "public-read"
+  attach_policy            = true
+  ignore_public_acls       = false
+  restrict_public_buckets  = false
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerPreferred"
+  force_destroy            = true
 }
