22729c891c3fdced6b0d630b181ee3b512ebb23b
Samuel Adetunji <guysnacho2@gmail.com>
2024-07-17 02:33:50 -0500

    Make outputs sensitive

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,11 @@
 output "s3-bucket-region" {
   value       = module.s3-bucket.s3_bucket_region
   description = "Current S3 region"
+  sensitive = true
 }
 
 output "s3-bucket-arn" {
   value       = module.s3-bucket.s3_bucket_arn
   description = "Current S3 arn"
+  sensitive = true
 }
