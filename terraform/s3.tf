002f755e271dd4224ea85d47f14c41d186071436
Samuel Adetunji <guysnacho2@gmail.com>
2024-07-17 04:13:45 -0500

    Test final changes with ubuntu

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -20,7 +20,6 @@ module "s3-bucket" {
       }
     ]
   })
-  block_public_acls       = false
   attach_public_policy    = true
   restrict_public_buckets = false
   object_ownership        = "BucketOwnerPreferred"
