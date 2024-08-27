189d7f4d6276f82cc897df7e8ed74ad858d30b5c
Samuel Adetunji <guysnacho2@gmail.com>
2024-08-27 05:21:47 -0500

    Cleanup cloudfront module

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -8,47 +8,11 @@ module "cloudfront" {
   #   staging = true
 
   create_origin_access_control = true
-  # origin_access_control = {
-  #   s3_oac = {
-  #     description      = "CloudFront access to S3"
-  #     origin_type      = "s3"
-  #     signing_behavior = "always"
-  #     signing_protocol = "sigv4"
-  #   }
-  # }
-
   origin = {
-
-    # appsync = {
-    #   domain_name = module.s3_bucket.s3_bucket_bucket_domain_name
-    #   custom_origin_config = {
-    #     http_port              = 80
-    #     https_port             = 443
-    #     origin_protocol_policy = "match-viewer"
-    #     origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-    #   }
-
-    #   custom_header = [
-    #     {
-    #       name  = "X-Forwarded-Scheme"
-    #       value = "https"
-    #     },
-    #     {
-    #       name  = "X-Frame-Options"
-    #       value = "SAMEORIGIN"
-    #     }
-    #   ]
-
-    #   origin_shield = {
-    #     enabled              = true
-    #     origin_shield_region = "us-west-2"
-    #   }
-    # }
-
     s3_oac = { # with origin access control settings (recommended)
       domain_name              = module.s3_bucket.s3_bucket_bucket_regional_domain_name
-      origin_access_control    = "s3_oac"                                               # key in `origin_access_control`
-      origin_access_control_id = aws_cloudfront_origin_access_control.cloudfront_oac.id # "E345SXM82MIOSU" # external OAС resource
+      origin_access_control    = "s3_oac"
+      origin_access_control_id = aws_cloudfront_origin_access_control.cloudfront_oac.id # external OAС resource
       origin_ssl_protocols     = ["TLSv1", "TLSv1.1", "TLSv1.2"]
       origin_shield = {
         enabled              = true
@@ -58,8 +22,8 @@ module "cloudfront" {
 
     s3_oac2 = { # with origin access control settings (recommended)
       domain_name              = module.failover_s3_bucket.s3_bucket_bucket_regional_domain_name
-      origin_access_control    = "s3_oac2"                                              # key in `origin_access_control`
-      origin_access_control_id = aws_cloudfront_origin_access_control.cloudfront_oac.id # "E345SXM82MIOSU" # external OAС resource
+      origin_access_control    = "s3_oac2"
+      origin_access_control_id = aws_cloudfront_origin_access_control.cloudfront_oac.id
       origin_ssl_protocols     = ["TLSv1", "TLSv1.1", "TLSv1.2"]
       origin_shield = {
         enabled              = true
@@ -67,14 +31,6 @@ module "cloudfront" {
       }
     }
   }
-
-  # origin_group = {
-  #   group_one = {
-  #     failover_status_codes      = [403, 404, 500, 502]
-  #     primary_member_origin_id   = "s3_oac"
-  #     domain_name                = module.s3_bucket.s3_bucket_bucket_regional_domain_name
-  #   }
-  # }
 
   default_cache_behavior = {
     target_origin_id       = "s3_oac"
