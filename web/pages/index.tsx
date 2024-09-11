b1980f36ac1b289d456267291179c3abdf9e7bab
Samuel Adetunji <guysnacho2@gmail.com>
2024-09-11 04:07:47 -0500

    Add more vinyl

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -10,17 +10,29 @@ export type ProductProps = {
 };
 const productList: ProductProps[] = [
   {
-    name: "Bando Stone and The New World Vinyl",
+    name: "Bando Stone and The New World",
     price: 45.15,
     thumbnail:
       "https://t2.genius.com/unsafe/728x0/https%3A%2F%2Fimages.genius.com%2Ff19320aae82a75396d97def01ae89ff3.1000x1000x1.png",
+  },
+  {
+    name: "alligator bites never heal - Doechii",
+    price: 30,
+    thumbnail:
+      "https://shop.capitolmusic.com/cdn/shop/files/DoechiiABNHLPInsert.png?v=1724951711&width=800",
+  },
+  {
+    name: "Nova - James Fauntleroy, Terrace Martin",
+    price: 30.99,
+    thumbnail:
+      "https://images.squarespace-cdn.com/content/v1/5699291fa976afc919dbca7d/a701db3b-83c1-457b-a892-0c46b0e6749c/Nova+Artwork.jpg?format=500w",
   },
 ];
 
 export default function Home() {
   return (
     <div className="container mx-auto">
-      <div className="w-4/5 mx-auto">
+      <div className="flex flex-wrap w-4/5 mx-auto justify-evenly gap-5">
         {productList.map((item) => (
           <Product
             key={item.name}
