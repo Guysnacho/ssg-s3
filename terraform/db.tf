32198fe36d696d68d6c2650dc418cf8a22ef20b5
Samuel Adetunji <guysnacho2@gmail.com>
2024-09-13 13:45:41 -0500

    bruh
    - Add vpc and throw db into it.
    - allow ingress and egress (hopefully)
    - disable invalid flags, manual username/password
    - vpc with public, private, and db subnets
    + ingress/egress blocks

--- terraform/db.tf
+++ terraform/db.tf
@@ -5,34 +5,42 @@ module "db" {
   identifier = "storefront-db"
 
   engine               = "postgres"
-  engine_version       = "16"
   family               = "postgres16" # DB parameter group
-  major_engine_version = "16"         # DB option group
+  engine_version       = "16"
+  major_engine_version = "16" # DB option group
   instance_class       = "db.m5.large"
 
   allocated_storage     = 5
   max_allocated_storage = 7
 
-  db_name  = var.db-name
-  username = var.db-username
-  port     = 5432
+  db_name                     = var.db-name
+  username                    = var.db-username
+  password                    = var.db-password
+  manage_master_user_password = false
+  port                        = 5432
 
   enabled_cloudwatch_logs_exports = ["postgresql"]
   create_cloudwatch_log_group     = true
   storage_type                    = "gp2"
 
-  # db_subnet_group_name   = module.vpc.database_subnet_group
-  # vpc_security_group_ids = [module.security_group.security_group_id]
-
-  blue_green_update = {
-    enabled = true
-  }
-  parameters = [
-    # required for blue-green deployment
-    {
-      name         = "rds.logical_replication"
-      value        = 1
-      apply_method = "pending-reboot"
-    }
-  ]
+  db_subnet_group_name        = module.vpc.database_subnet_group
+  vpc_security_group_ids      = [module.security_group.security_group_id]
+  subnet_ids                  = module.vpc.public_subnets
+  db_subnet_group_description = "DB Subnet"
+  publicly_accessible         = true
+  network_type                = "IPV4"
+  depends_on                  = [module.vpc]
+  # Databases using Secrets Manager are not currently supported for Blue Green Deployments
+  # blue_green_update = {
+  #   enabled = true
+  # }
+  # parameters = [
+  #   # required for blue-green deployment
+  #   {
+  #     name         = "rds.logical_replication"
+  #     value        = 1
+  #     apply_method = "pending-reboot"
+  #   }
+  # ]
+  skip_final_snapshot = true
 }
---
+++ terraform/vpc.tf
@@ -0,0 +1,50 @@
+locals {
+  vpc_cidr = "10.0.0.0/16"
+  azs      = ["us-west-2a", "us-west-2b"]
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${var.db-username}_vpc"
+  cidr = local.vpc_cidr
+
+  azs              = local.azs
+  public_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  private_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 3)]
+  database_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 6)]
+
+  create_database_internet_gateway_route = true
+  create_database_nat_gateway_route      = true
+}
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  name        = "${var.db-username}_security_group"
+  description = "Complete PostgreSQL example security group"
+  vpc_id      = module.vpc.vpc_id
+
+  # ingress
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 5432
+      to_port     = 5432
+      protocol    = "tcp"
+      description = "PostgreSQL access from within VPC"
+      cidr_blocks = module.vpc.vpc_cidr_block
+      # cidr_blocks = ["0.0.0.0/0"]
+    },
+  ]
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 5432
+      to_port     = 5432
+      protocol    = "tcp"
+      description = "PostgreSQL access from within VPC"
+      cidr_blocks = module.vpc.vpc_cidr_block
+    },
+  ]
+}
