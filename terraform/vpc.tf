4e1d461b87416ffaecec56840f534ec1faf0681b
Samuel Adetunji <guysnacho2@gmail.com>
2024-09-15 02:48:04 -0500

    Be specific about cidr's

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,11 +1,13 @@
 locals {
-  vpc_cidr = "10.0.0.0/16"
+  # vpc_cidr = "10.0.0.0/16"
+  vpc_cidr = "192.0.0.0/16"
+  port     = 5432
   azs      = ["us-west-2a", "us-west-2b"]
 }
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
+  version = "5.13.0"
 
   name = "storefront_vpc"
   cidr = local.vpc_cidr
@@ -16,34 +18,39 @@ module "vpc" {
   database_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 6)]
 
   create_database_internet_gateway_route = true
-  create_database_nat_gateway_route      = true
 }
 
 module "security_group" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "~> 5.0"
+  version = "5.2.0"
 
-  name        = "${var.db-username}_security_group"
+  name        = "storefront_security_group"
   description = "Complete PostgreSQL example security group"
   vpc_id      = module.vpc.vpc_id
+
 
   # ingress
   ingress_with_cidr_blocks = [
     {
+      action      = "allow"
       from_port   = 5432
       to_port     = 5432
       protocol    = "tcp"
-      description = "PostgreSQL access from within VPC"
+      rule_action = "allow"
+      rule_number = 100
+      description = "PostgreSQL ingress access from within VPC"
       cidr_blocks = module.vpc.vpc_cidr_block
-      # cidr_blocks = ["0.0.0.0/0"]
     },
   ]
   egress_with_cidr_blocks = [
     {
+      action      = "allow"
       from_port   = 5432
       to_port     = 5432
-      protocol    = "tcp"
-      description = "PostgreSQL access from within VPC"
+      protocol    = "-1"
+      rule_action = "allow"
+      rule_number = 100
+      description = "PostgreSQL egress access from within VPC"
       cidr_blocks = module.vpc.vpc_cidr_block
     },
   ]
