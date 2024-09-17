9382c23079863d1f98b015e4c16473f9777fe3d9
Samuel Adetunji <guysnacho2@gmail.com>
2024-09-17 03:18:24 -0500

    Add environmental variables for db access

--- terraform/auth_lambda.tf
+++ terraform/auth_lambda.tf
@@ -11,7 +11,17 @@ module "auth_lambda" {
   runtime       = "nodejs20.x"
   handler       = "auth.handler"
   publish       = true
+  environment_variables = {
+    db_host     = module.db.cluster_endpoint
+    db_username = var.db-username
+    db_password = module.db.cluster_master_password
+    db_name     = var.db-name
+    secret      = var.cloudfront_secret
+  }
 
   # use_existing_cloudwatch_log_group = true
-  source_path = "${path.module}/lib/auth.js"
+  source_path            = "${path.module}/lib/auth.js"
+  architectures          = ["arm64"]
+  vpc_subnet_ids         = module.vpc.public_subnets
+  vpc_security_group_ids = [module.security_group.security_group_id]
 }
