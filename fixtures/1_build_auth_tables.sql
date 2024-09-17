3658632702dce3030f423271d4b4f523a4abf39a
Samuel Adetunji <guysnacho2@gmail.com>
2024-09-17 03:08:31 -0500

    Add setup SQL statements

---
+++ fixtures/1_build_auth_tables.sql
@@ -0,0 +1,19 @@
+-- ALWAYS KEEP THESE SEPARATE IN REAL LIFE
+-- CREATE TABLE member (id UUID PRIMARY KEY DEFAULT gen_random_uuid ())
+-- CREATE TABLE auth (
+--     id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+--     email TEXT NOT NULL,
+--     password TEXT NOT NULL,
+--     fname TEXT NOT NULL,
+--     lname TEXT NOT NULL,
+-- )
+
+CREATE TABLE member (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL
+);
+
+SELECT * from public.member;
