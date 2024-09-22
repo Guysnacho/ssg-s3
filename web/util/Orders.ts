3af8be0bae432bd12e1366769abf3c0a455ec17d
Samuel Adetunji <guysnacho2@gmail.com>
2024-09-22 15:29:36 -0500

    Clean up order request

--- web/util/Orders.ts
+++ web/util/Orders.ts
@@ -1,5 +1,17 @@
 import { ProductProps } from "@/pages";
 
-export const handlePurchase = (product: ProductProps) => {
-  alert(`Purchasing ${product.name} for $${product.price}`);
+export const handlePurchase = (user_id: string, product: ProductProps) => {
+  console.debug(`Purchasing ${product.name} for $${product.price}`);
+  fetch(`${process.env.NEXT_PUBLIC_APIGW}/sale`, {
+    method: "POST",
+    body: JSON.stringify({ quantity: 1, sku: product.sku, user_id }),
+  })
+    .then(async (res) => {
+      const body = await res.json();
+      console.debug(body);
+    })
+    .catch((err) => {
+      console.error(err);
+      alert("Failed to complete your purchase. Please try again later.");
+    });
 };
