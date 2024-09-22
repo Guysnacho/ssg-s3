61cab3df5df66b95f030d7354837759531a10716
Samuel Adetunji <guysnacho2@gmail.com>
2024-09-22 12:21:14 -0500

    So that didn't work.

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -50,18 +50,18 @@ module "cloudfront" {
     allowed_methods        = ["GET", "HEAD", "OPTIONS"]
     cached_methods         = ["GET", "HEAD"]
 
-    lambda_function_association = {
-      # Valid keys: viewer-request, origin-request, viewer-response, origin-response
-      viewer-response = {
-        lambda_arn   = module.auth_lambda.lambda_function_qualified_arn
-        include_body = true
-      }
+    # lambda_function_association = {
+    #   # Valid keys: viewer-request, origin-request, viewer-response, origin-response
+    #   viewer-response = {
+    #     lambda_arn   = module.auth_lambda.lambda_function_qualified_arn
+    #     include_body = true
+    #   }
 
-      viewer-request = {
-        lambda_arn   = module.sale_lambda.lambda_function_qualified_arn
-        include_body = true
-      }
-    }
+    #   viewer-request = {
+    #     lambda_arn   = module.sale_lambda.lambda_function_qualified_arn
+    #     include_body = true
+    #   }
+    # }
 
     use_forwarded_values = true
   }
