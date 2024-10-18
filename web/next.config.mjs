3c823258dd811b6cfd69545514d254f93a80d4f6
Samuel Adetunji <guysnacho2@gmail.com>
2024-10-18 01:23:36 -0500

    Containerize app, Dockerfile and ignore

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+  output: "standalone",
 };
 
 export default nextConfig;
