7fbfa88de13bb29f59c38450eec7ef149a544923
Guysnacho <tunjiprod@gmail.com>
2025-04-03 01:56:19 -0500

    Update support

---
+++ fixtures/README.md
@@ -0,0 +1,10 @@
+# Useful and easy to access files
+
+## SQL
+
+Scripts used to manually seed data into our RDS instance.
+You can run the scripts from any Postgres client as long as you have access to Secrets Manager where you can find the DB username and password.
+
+## JSON
+
+Example JSON bodies used for our lambdas.
--- terraform/lib/testbench.js
+++ terraform/lib/testbench.js
@@ -1,6 +1,9 @@
 /**
  * This file helps you test lambda handler code locally. Good ol manual testing.
  * Runnable with `node .\testbench.js` from the command line (assuming you have node installed)
+ * 
+ * How To - Install all packages used in the lambda locally, they aren't already because AWS
+ * provides them in the lambda environment`npm i <package_name>`
  */
 
 const { handler } = require("./auth/index");
